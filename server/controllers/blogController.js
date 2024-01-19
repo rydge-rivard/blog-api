b1596d79f73447d75a3d3155b686a632fd68f436
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2024-01-19 09:00:33 -0500

    Update blog API to send data from MongoDB.

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -3,47 +3,16 @@ const Blog = require("../models/blog");
 const Comment = require("../models/comment");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  // const blogs = Blog.find().sort({ date: -1 }).exec();
-  const data = [
-    {
-      title: "test name one",
-      content: "test content",
-      date: Date(),
-      published: true,
-      id: 101,
-    },
-    {
-      title: "test name two",
-      content: "test content",
-      date: Date(),
-      published: true,
-      id: 102,
-    },
-    {
-      title: "test name three",
-      content: "test content",
-      date: Date(),
-      published: true,
-      id: 103,
-    },
-  ];
+  const blogs = await Blog.find().sort({ date: -1 }).exec();
 
-  res.json(data);
+  res.json(blogs);
 });
 
 exports.blog_detail = asyncHandler(async (req, res, next) => {
-  // const blogs = Blog.find().sort({ date: -1 }).exec();
-  // also find comments by blog id
-  const data = [
-    {
-      title: "test name one",
-      content:
-        "For now, it is acceptable to just allow access from any origin. This makes development quite a bit easier but for any real project, once you deploy to a production environment you will probably want to specifically block access from any origin except your frontend website. The documentation above explains how to do this.",
-      date: Date(),
-      published: true,
-      id: 101,
-    },
-  ];
+  const [blog, comments] = await Promise.all([
+    Blog.findById(req.params.id).sort({ date: -1 }).exec(),
+    Comment.find({ blog_id: req.params.id }).sort({ date: -1 }).exec(),
+  ]);
 
-  res.json(data);
+  res.json({ blog: blog, comments: comments });
 });
